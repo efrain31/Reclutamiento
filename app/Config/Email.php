381b40b2774895cef8becf63e6fa6c8939dbc582
brizeida <brizeidarosales@geovoy.com>
2025-04-02 13:54:05 -0600

    cambios de estilos v.1.0.4

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -18,8 +18,8 @@ class Email extends BaseConfig
     /**
      * The mail sending protocol: mail, sendmail, smtp
      */
-    public string $protocol = 'mail';
-
+    //public string $protocol = 'mail';
+    public string $protocol	= 'smtp';
     /**
      * The server path to Sendmail.
      */
@@ -28,23 +28,25 @@ class Email extends BaseConfig
     /**
      * SMTP Server Hostname
      */
-    public string $SMTPHost = '';
+    public string $SMTPHost	= 'smtp.mailtrap.io';
+   // public string $SMTPHost = '';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = '';
-
+    //public string $SMTPUser = '';
+    public string $SMTPUser	= '2595e9c50103b0';
     /**
      * SMTP Password
      */
-    public string $SMTPPass = '';
+    //public string $SMTPPass = '';
+    public string $SMTPPass	= 'c053bd6ba1179a';
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 25;
-
+    //public int $SMTPPort = 25;
+    public int $SMTPPort = 587;
     /**
      * SMTP Timeout (in seconds)
      */
@@ -62,7 +64,7 @@ class Email extends BaseConfig
      *             to the server. 'ssl' means implicit SSL. Connection on port
      *             465 should set this to ''.
      */
-    public string $SMTPCrypto = 'tls';
+    public string $SMTPCrypto = 'ssl';
 
     /**
      * Enable word-wrap
@@ -77,7 +79,7 @@ class Email extends BaseConfig
     /**
      * Type of mail, either 'text' or 'html'
      */
-    public string $mailType = 'text';
+    public string $mailType = 'html';
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
